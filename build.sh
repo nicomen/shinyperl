538cab9b8fce925b090dd314d8b7bd3086c8677f
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-06-14 00:20:59 +0200

    Try deploying from subdir after building and copying to it

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tpage --pre_chomp --post_chomp --eval_perl index.tt > index.html
+mkdir -p dist
+tpage --pre_chomp --post_chomp --eval_perl index.tt > dist/index.html
+cp perl.svg script.js style.css dist/
