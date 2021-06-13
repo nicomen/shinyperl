62afd5e676c6888caecb3c850146b7a5b95dcf8d
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-06-14 01:24:08 +0200

    More explanatory

--- snippets/Set boundaries for a value.pl
+++ snippets/Set boundaries for a value.pl
@@ -9,11 +9,11 @@ my @inputs = (-10,10,200);
 for my $i (@inputs) {
   my $value = (sort { $a <=> $b } ($min, $max, $i))[1];
 
-  say "$value is within ($min,$max)";
+  say "$i set to $value to be within ($min,$max)";
 }
 
-# 1 is within (1,100)
-# 10 is within (1,100)
-# 100 is within (1,100)
+# -10 set to 1 to be within (1,100)
+# 10 set to 10 to be within (1,100)
+# 200 set to 100 to be within (1,100)
 
 #! [% END %]
