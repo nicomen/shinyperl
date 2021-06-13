e41f2be7824510b68b217a2450834827ad6e1e6e
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-06-14 01:21:16 +0200

    Seems CNAME must be part of the gh-pages branch too

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ DIST_DIR=public
 
 mkdir -p $DIST_DIR
 tpage --pre_chomp --post_chomp --eval_perl index.tt > $DIST_DIR/index.html
-cp perl.svg script.js style.css $DIST_DIR/
+cp perl.svg script.js style.css CNAME $DIST_DIR/
