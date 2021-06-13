ab3e656767f2dc89f46fef42e1cbdcf0cef299e3
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-06-14 00:20:13 +0200

    Use two spaces indentation

--- snippets/Objects with Object::Pad.pl
+++ snippets/Objects with Object::Pad.pl
@@ -3,21 +3,21 @@
 use Object::Pad;
 
 class Point {
-   has $x = 0;
-   has $y = 0;
+  has $x = 0;
+  has $y = 0;
 
-   BUILD {
-     ($x, $y) = @_;
-   }
+  BUILD {
+    ($x, $y) = @_;
+  }
 
-   method move ($dX, $dY) {
-      $x += $dX;
-      $y += $dY;
-   }
+  method move ($dX, $dY) {
+    $x += $dX;
+    $y += $dY;
+  }
 
-   method describe {
-      print "A point at ($x,$y)";
-   }
+  method describe {
+    print "A point at ($x,$y)";
+  }
 }
 
 Point->new(5,10)->describe; # A point at (5,10)
