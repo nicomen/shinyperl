92ff8b64e9fc92e41d979310e192cc1578def440
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-06-14 01:45:23 +0200

    Create CNAME while building, move source files into src dir

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+SRC_DIR=src
 DIST_DIR=public
 
 mkdir -p $DIST_DIR
-tpage --pre_chomp --post_chomp --eval_perl index.tt > $DIST_DIR/index.html
-cp perl.svg script.js style.css CNAME $DIST_DIR/
+echo "shinyperl.com" > $DIST_DIR/CNAME
+tpage --pre_chomp --post_chomp --eval_perl $SRC_DIR/index.tt > $DIST_DIR/index.html
+for a in style.css perl.svg script.js; do
+  cp $SRC_DIR/$a $DIST_DIR/;
+done;
