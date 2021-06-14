60e95a5d9a3b1ec8d1aeeaeafc4b59ade9afef48
sudo-batman <gauravrai7860@gmail.com>
2021-06-14 12:48:08 +0530

    Create Data Visualization using Chart::Plotly.pl

---
+++ snippets/Data Visualization using Chart::Plotly.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl -W # [% title = 'Data Visualization using Chart::Plotly - Line chart'; BLOCK code %]
+
+use strict;
+use warnings;
+
+use Chart::Plotly 'show_plot';
+use Chart::Plotly::Image 'save_image';
+use Chart::Plotly::Plot;
+use Chart::Plotly::Trace::Scatter;
+
+my %domainAxis = ("data" => ["2021-04-15", "2021-04-16", "2021-04-17", "2021-04-18"], "label" => "Date");
+my %rangeAxis  = ("data" => [10, 3, 5, 9], "legendName" => "Automobiles Sold");
+
+my $plot    = Chart::Plotly::Plot->new();
+# For multi-line chart create it in loop
+my $scatter = Chart::Plotly::Trace::Scatter->new(
+    x    => $domainAxis{data},
+    y    => $rangeAxis{data},
+    name => $rangeAxis{legendName}
+);
+$plot->add_trace($scatter);
+
+# Opens the plot in a browser locally
+show_plot($plot);
+
+# Save the generated image locally
+save_image(
+    file   => "lineChart.png",    # Referring to a local filesystem path
+    plot   => $plot,
+    width  => 1024,               # Sets the image width
+    height => 768,                # Sets the image height
+    engine => 'auto'
+);
+
+#! [% END %]
