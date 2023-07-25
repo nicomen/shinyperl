00417ad3b00881786a9577908ae4205bc88d8e8a
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2023-07-25 14:00:36 +0200

    Update and rename Set boundaries for a value.pl to Clamping.pl
    Use a function and use the name `clamp` as that seems to be common wording.

--- snippets/Clamping.pl
+++ snippets/Clamping.pl
@@ -1,15 +1,17 @@
-#!/usr/bin/env perl -W # [% title = 'Set boundaries for a value'; BLOCK code %]
+#!/usr/bin/env perl -W # [% title = 'Clamping a value'; BLOCK code %]
 
-use v5.10;
+use v5.36;
 
 my ($min, $max) = (1,100);
-
 my @inputs = (-10,10,200);
 
 for my $i (@inputs) {
-  my $value = (sort { $a <=> $b } ($min, $max, $i))[1];
-
+  my $value = clamp($i, $min, $max);
   say "$i set to $value to be within ($min,$max)";
+}
+
+sub clamp ($i,$min,$max) {
+  return (sort { $a <=> $b } ($min, $max, $i))[1];
 }
 
 # -10 set to 1 to be within (1,100)
